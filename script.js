e0a4ea385c5d4d0d9254ca4639eda8a874a0927a
“Dema10” <gabrieledematteis@me.com>
2024-03-02 01:05:12 +0100

    modified

--- script.js
+++ script.js
@@ -31,19 +31,48 @@ newIndirizzo.addEventListener("click", function () {
 // ESERCIZIO 10: Scrivi una funzione per aggiungere una
 // classe CSS ad ogni link Amazon della tabella
 
-let aggiungi = document.querySelectorAll(".linkAmazonPiu");
+let aggiungi = document.querySelectorAll(".prodotti");
 
-aggiungi.addEventListener("mouseover", function () {
-    aggiungi.classList.add = ("linkAmazonPiu");
-});
-
-aggiungi.addEventListener("mouseout", function () {
-    aggiungi.classList.remove = ("linkAmazonPiu");
+aggiungi.forEach(function (prodotto) {
+    prodotto.addEventListener("mouseover", function () {
+        prodotto.style.boxShadow = "0 0 5px 5px lightgreen";
+    });
+    prodotto.addEventListener("mouseout", function () {
+        prodotto.style.boxShadow = "none";
+    });
 });
 
 // ESERCIZIO 11: Scrivi una funzione per aggiungere/togliere una 
 // classe CSS a tutte le immagini della tabella; questa classe deve 
 // modificare la visibilità/invisibilità dell'immagine
 
+let visibila = document.querySelectorAll(".image");
+
+visibila.forEach(function (visibile) {
+    visibile.addEventListener("mouseover", function () {
+        visibile.style.opacity = "0";
+    });
+    visibile.addEventListener("mouseout", function () {
+        visibile.style.opacity = "1";
+    });
+});
+
 // ESERCIZIO 12: Scrivi una funzione per cambiare il colore del prezzo
 // di ogni prodotto in uno differente, ogni volta che viene invocata
+
+let cambiaColorePrezzo = document.querySelectorAll(".price");
+let ismodNotte = false;
+
+function coloreCasuale() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + " , " + g + " , " + b + " )";
+} 
+
+for (let i = 0; i < cambiaColorePrezzo.length; i++) {
+    cambiaColorePrezzo[i].addEventListener("mouseover", function () {
+        cambiaColorePrezzo[i].style.color = coloreCasuale();
+     });
+}
+       
